8d0b91eecfe101d0b2e54e5c0fb013a025c9334e
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-01-18 01:43:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TELEGRAM_TOKEN = "8102131483:AAEd0RkoxXKtvVYXTmV5W8OeQWMtnPwwxX0"
 OPENAI_API_KEY = "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"
-
+GROQ_API_KEY = "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"
 ADMINS = [
     825443707,  # <-- замените на свой Telegram user_id
     1186183697,  # <-- при необходимости добавляйте ещё
